c8c68c369fe9239fd9e56a4d9556971043e20078
Albert WU <albertwu269@gmail.com>
2025-07-22 11:56:02 +0800

    Drive GO GO GO

--- 22-7-25/DriveTest/src/main.cpp
+++ 22-7-25/DriveTest/src/main.cpp
@@ -2,8 +2,8 @@
 
 const uint8_t ma1 = 10;
 const uint8_t ma2 = 11;
-const uint8_t mb1 = 30;
-const uint8_t mb2 = 40;
+const uint8_t mb1 = 7;
+const uint8_t mb2 = 3;
 
 void setup() {
     Serial.begin(9600);
@@ -20,22 +20,32 @@ void loop() {
     // Control motor A
     digitalWrite(ma1, HIGH);
     digitalWrite(ma2, LOW);
-    delay(1000); // Run motor A for 1 second
+    delay(200); // Run motor A for 1 second
     digitalWrite(ma1, LOW);
     digitalWrite(ma2, LOW);
+
+    delay(2000);
 
     digitalWrite(ma1, LOW);
     digitalWrite(ma2, HIGH);
-    delay(1000); // Run motor A in reverse for 1 second
+    delay(200); // Run motor A in reverse for 1 second
     digitalWrite(ma1, LOW);
     digitalWrite(ma2, LOW);
 
-    // // Control motor B
-    // digitalWrite(mb1, HIGH);
-    // digitalWrite(mb2, LOW);
-    // delay(1000); // Run motor B for 1 second
-    // digitalWrite(mb1, LOW);
-    // digitalWrite(mb2, LOW);
+    // Control motor B
+    digitalWrite(mb1, HIGH);
+    digitalWrite(mb2, LOW);
+    delay(1000); // Run motor B for 1 second
+    digitalWrite(mb1, LOW);
+    digitalWrite(mb2, LOW);
+
+    delay(2000);
+
+    digitalWrite(mb1, LOW);
+    digitalWrite(mb2, HIGH);
+    delay(200); // Run motor A in reverse for 1 second
+    digitalWrite(mb1, LOW);
+    digitalWrite(mb2, LOW);
 
     Serial.println("Motors stopped.");
     delay(2000); // Wait before next iteration
