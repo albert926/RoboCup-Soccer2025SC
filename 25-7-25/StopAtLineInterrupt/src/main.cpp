5c681f7eb2d33ab1348f0870e5c76d2fd64a1a34
Albert WU <albertwu269@gmail.com>
2025-07-25 13:21:14 +0800

    Small error with else

--- 25-7-25/StopAtLineInterrupt/src/main.cpp
+++ 25-7-25/StopAtLineInterrupt/src/main.cpp
@@ -12,7 +12,10 @@ const uint8_t ma2 = 2;
 const uint8_t mb1 = 5;
 const uint8_t mb2 = 6;
 
+const uint8_t abortLED = 28;
+
 const uint8_t startInterruptPin = 9;
+uint8_t roboStat = 1; // 1 for running, 0 for stopped
 
 volatile bool InterStat = 0;
 
@@ -28,22 +31,23 @@ void setup() {
     pinMode(ma2, OUTPUT);
     pinMode(mb1, OUTPUT);
     pinMode(mb2, OUTPUT);
+    pinMode(abortLED, OUTPUT);
     pinMode(startInterruptPin, INPUT_PULLUP);
 
     while(digitalRead(startInterruptPin) == HIGH){
     }
     Serial.println("HI!");
 
-    attachInterrupt(0, toggleInter, FALLING);
+    attachInterrupt(9, toggleInter, FALLING);
 }
 
 void loop() {
-    Serial.println(F("Waiting for interrupt..."));
-    while(!InterStat) {
-      if (InterStat) {
-          InterStat = false;
-          Serial.println(F("Interrupt triggered!"));
-      }
+    if(InterStat) {
+      roboStat = 0; // Stop the robot
+      digitalWrite(abortLED, 255);
+      Stop();
+    } else {
+      roboStat = 1; // Continue running
     }
   }
 
